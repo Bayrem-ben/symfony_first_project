be85ddeae4488a8b1d7c47d66cfd5da33ac56398
bayrem-ben <bayrem.benabdallah@gmail.com>
2022-01-18 16:48:27 +0100

    symfonycast

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,24 +2,42 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
-    public function homepage()
+    public function homepage(Environment $twigEnv)
     {
-        return new Response('What a controller we have!');
+        /*
+        $html = $twigEnv -> render("question/homepage.html.twig");
+        return new Response($html);
+        */
+        return $this->render('question/homepage.html.twig');
     }
-    
     /**
-     * @Route("/questions/{slug}")
+     * @Route("/questions/{slug}", name="app_question_show")
      */
     public function show($slug)
     {
-        return new Response(sprintf(ucwords(str_replace('-', ' ', $slug))));
+        $answers = [
+            "piece of shit",
+            "the man the legend",
+            "the m fucker"
+        ];
+
+        dump($this);
+
+        return $this->render('question/show.html.twig', 
+        [
+            "question" => ucwords(str_replace('-', ' ', $slug)),
+            "answers" => $answers
+        ]);
+        //return new Response(sprintf(ucwords(str_replace('-', ' ', $slug))));
     }
 }
